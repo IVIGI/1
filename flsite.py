652d68b253301ff7557c46332b32110ed01e0867
iiila <xa4as2001@gmail.com>
2024-05-21 09:34:31 +0300

    Изначальная система

--- flsite.py
+++ flsite.py
@@ -18,6 +18,21 @@ loaded_model_Log = pickle.load(open('model2/Iris_pickle_file', 'rb'))
 loaded_model_Tree = pickle.load(open('model3/Iris_pickle_fileTREE', 'rb'))
 
 
+metrics_classification_data = [
+{"name": "Confusion matrix", "type": "logistic", "value": {"Predicted": [2, 1, 1, 37.4,19.4,178,3900,2007], "Actual": [2, 1, 1, 37.8,19.2,179,3910,2007]}},
+{"name": "Confusion matrix", "type": "knn", "value": {"Predicted": [3, 2, 2, 39.8,21.7,182,4100,2007], "Actual": [3, 2, 2, 38.4,18.4,182,4000,2007]}},
+{"name": "Confusion matrix", "type": "Tree", "value": {"Predicted": [6, 1, 1, 41.4,23.3,176,4200,2007], "Actual": [5, 1, 1, 41.6,23.4,180,4150,2007]}},
+{"name": "Accuracy", "type": "logistic", "value": 0.8611111111111112},
+{"name": "Accuracy", "type": "KNN", "value": 0.8333333333333334},
+{"name": "Accuracy", "type": "Tree", "value": 0.7833333333333333},
+{"name": "Precision", "type": "logistic", "value": 0.845360824742268},
+{"name": "Precision", "type": "KNN", "value": 0.8297872340425532},
+{"name": "Precision", "type": "Tree", "value": 0.797752808988764},
+{"name": "Recall", "type": "logistic", "value": 0.8913043478260869},
+{"name": "Recall", "type": "KNN", "value": 0.8478260869565217},
+{"name": "Recall", "type": "Tree", "value": 0.7717391304347826},
+]
+
 @app.route("/")
 def index():
     return render_template('index.html', title="Лабораторные работы, выполненные Игнатьев Артем Андреевич", menu=menu)
@@ -26,7 +41,7 @@ def index():
 @app.route("/p_knn", methods=['POST', 'GET'])
 def f_lab1():
     if request.method == 'GET':
-        return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu, class_model='')
+        return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu, class_model='', parametr=metrics_classification_data)
     if request.method == 'POST':
         X_new = np.array([[int(request.form['list1']),
                            int(request.form['list2']),
@@ -39,12 +54,12 @@ def f_lab1():
         pred = loaded_model_knn.predict(X_new)
         gender = gender_dict[pred[0]]
         return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu,
-                               class_model="Это: " + gender)
+                               class_model="Это: " + gender, parametr = metrics_classification_data )
 
 @app.route("/p_lab2", methods=['POST', 'GET'])
 def f_lab2():
     if request.method == 'GET':
-        return render_template('lab2.html', title="Логистическая регрессия", menu=menu)
+        return render_template('lab2.html', title="Логистическая регрессия", menu=menu,parametr = metrics_classification_data )
     if request.method == 'POST':
         X_new = np.array([[int(request.form['list1']),
                            int(request.form['list2']),
@@ -57,12 +72,12 @@ def f_lab2():
         pred = loaded_model_Log.predict(X_new)
         gender = gender_dict[pred[0]]
         return render_template('lab2.html', title="Логистическая регрессия", menu=menu,
-                               class_model="Это: " + gender)
+                               class_model="Это: " + gender, parametr = metrics_classification_data  )
 
 @app.route("/p_lab3")
 def f_lab3():
     if request.method == 'GET':
-        return render_template('lab3.html', title="Дерево решений", menu=menu)
+        return render_template('lab3.html', title="Дерево решений", menu=menu,parametr = metrics_classification_data )
     if request.method == 'POST':
         X_new = np.array([[int(request.form['list1']),
                            int(request.form['list2']),
@@ -75,7 +90,7 @@ def f_lab3():
         pred = loaded_model_Tree.predict(X_new)
         gender = gender_dict[pred[0]]
         return render_template('lab3.html', title="Дерево решений", menu=menu,
-                               class_model="Это: " + gender)
+                               class_model="Это: " + gender, parametr = metrics_classification_data )
 
 @app.route('/api_v2',methods=['get'])
 def get_sort_v2():
@@ -92,6 +107,8 @@ def get_sort_v2():
     response = Response(response=json.dumps({'gender': gender}, ensure_ascii=False).encode('utf8'),
                         status=200,
                         mimetype='application/json; charset=utf-8')
+
     return response
+
 if __name__ == "__main__":
     app.run(debug=True)
